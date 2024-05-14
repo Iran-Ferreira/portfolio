ea91bda4df2b60349decd0b73832e029f01dd31b
Iran Ferreira <iranferreira712@gmail.com>
2024-05-14 17:39:17 -0300

    feat: social buttons

---
+++ src/app/components/social-btns/social-btns.tsx
@@ -0,0 +1,19 @@
+import { GitHubIcon } from "../icons/github-icon";
+import { InstaIcon } from "../icons/insta-icon";
+import { LinkedInIcon } from "../icons/linkedin-icon";
+
+export function SocialBtns(){
+    return (
+        <div className="social">
+            <a href="https://www.instagram.com/iran_ferreira712/">
+                <InstaIcon/>
+            </a>
+            <a href="https://www.linkedin.com/in/iran-ferreira/">
+                <LinkedInIcon/>
+            </a>
+            <a href="https://github.com/Iran-Ferreira">
+                <GitHubIcon/>
+            </a>
+        </div>
+    )
+}
