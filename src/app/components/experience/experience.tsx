a92f9acd63e69ac720b7dfd8ea0f72d7cc9208c9
Iran Ferreira <iranferreira712@gmail.com>
2024-06-21 09:10:03 -0300

    adding paragrafo

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -6,8 +6,9 @@ import { Skill } from '../skill/skill';
 export function Experience(){
     return(
         <div className="experience">
-            <SectionTitle text="Experience"/>
-            <p>Texto para colocar depois</p>
+            <SectionTitle text="Sobre"/>
+            <p>Em busca da primeira vaga como programador, me formei como técnico em Informática pelo IFRN Parelhas e 
+                lá pude aprender programação e ter gosto pela tecnologia.</p>
             <div className="experience-time">
                 <Skill image="/react.png" measure={2} years="2 anos"/>
                 <Skill image="/ts.png" measure={2} years="2 anos"/>
