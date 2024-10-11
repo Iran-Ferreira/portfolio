974fef327f6368cdd3e089ea211802859e827d04
Iran Ferreira <iranferreira712@gmail.com>
2024-10-11 19:57:57 -0300

    fix: new import

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -2,6 +2,8 @@ import { GitHubIcon } from "../icons/github-icon";
 import { InstaIcon } from "../icons/insta-icon";
 import { LinkedInIcon } from "../icons/linkedin-icon";
 
+import "./social-btns.scss"
+
 export function SocialBtns(){
     return (
         <div className="social">
