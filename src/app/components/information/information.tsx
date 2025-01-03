649cf0be708fec148415141f988e104169b6d3b0
Iran Ferreira <iranferreira712@gmail.com>
2025-01-03 15:41:55 -0300

    feat: new tag span and class educational info

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -4,15 +4,16 @@ import "./information.scss"
 export function Info(){
     return (
         <div className="infos">
-            <SectionTitle text="Linguages"/>
+            <SectionTitle text="Idiomas"/>
             <div className="languages-info">
                 <span>🇧🇷 PT-BR - Nativo</span>
                 <span>🇺🇸 Inglês - Intermediário</span>
             </div>
-            <SectionTitle text="Educação"/>
+            <SectionTitle text="Educação 🎓"/>
             <div className="educational-info">
-                <span>🎓</span>
                 <span>Técnico em Informática - IFRN Parelhas</span>
+                <span>Curso de Formação Inicial e Continuada (FIC) em sistemas embarcados do projeto de 
+                    residência em TIC 37 - IFRN - Campus Avançado Natal - Zona Leste</span>
           </div>
         </div>
     )
